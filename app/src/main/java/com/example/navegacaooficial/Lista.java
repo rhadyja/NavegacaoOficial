5525de57079c3293928017e996a9074cb643a885
Rhadyja Gabriela <rgob@ecomp.poli.br>
2022-09-09 14:17:24 -0300

    implementando class Lista

--- app/src/main/java/com/example/navegacaooficial/Lista.java
+++ app/src/main/java/com/example/navegacaooficial/Lista.java
@@ -1,4 +1,51 @@
 package com.example.navegacaooficial;
 
-public class Lista {
+public class Lista <T> {
+
+    private Node<T> inicio;
+    private int quantidade;
+
+    public void inserir(T novo) {
+        Node<T> novoNo = new Node<T>(novo);
+        if(inicio == null) {
+            inicio = novoNo;
+        }
+        else {
+            Node<T> aux = inicio;
+            while(aux.proximo != null) {
+                aux = aux.proximo;
+            }
+            aux.proximo= novoNo;
+        }
+        quantidade++;
+    }
+
+    public String toString(){
+        String s = "";
+        Node<T> aux = inicio;
+        while(aux != null) {
+            s = s + aux + ",";
+            aux = aux.proximo;
+        }
+        return "[" + s + "]";
+    }
+
+    public T consultar(int index) {
+        if(index < quantidade) {
+            int i = 0;
+            Node<T> aux = inicio;
+            while(i != index) {
+                aux = aux.proximo;
+                i++;
+            }
+            return aux.informacao;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
 }
