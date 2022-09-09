25a92a329be15103da17b68a8c9bf9f961f3278c
Rhadyja Gabriela <rgob@ecomp.poli.br>
2022-09-09 14:19:07 -0300

    implementando class AreaClicavel

--- app/src/main/java/com/example/navegacaooficial/AreaClicavel.java
+++ app/src/main/java/com/example/navegacaooficial/AreaClicavel.java
@@ -1,4 +1,71 @@
 package com.example.navegacaooficial;
 
 public class AreaClicavel {
+    private int atual;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private int proximo;
+
+    public AreaClicavel(){
+
+    }
+
+    public AreaClicavel(int atual, double x1, double y1, double x2, double y2, int proximo) {
+        this.atual = atual;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.proximo = proximo;
+    }
+
+    public int getAtual() {
+        return atual;
+    }
+
+    public void setAtual(int atual) {
+        this.atual = atual;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+    public int getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(int proximo) {
+        this.proximo = proximo;
+    }
 }
